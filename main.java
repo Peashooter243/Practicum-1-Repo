4ed201629001b512d3e9d7eead3c2c555ba9772f
Peashooter243 <alejandrobanda5604@gmail.com>
2024-02-12 21:56:12 -0500

    what

--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 // The Import Statement in Java
+//import java.util.Scanner;
 
-public class Main {
+public class main {
   
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    java.util.Scanner input = new java.util.Scanner(System.in);
 
     System.out.println("What is your name? ");
     String name = input.next();
